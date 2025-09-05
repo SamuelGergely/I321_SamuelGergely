4d0b587de1dabe44cf3c963d8cd4e04838209aeb
Samuel Gergely <143069755+SamuelGergely@users.noreply.github.com>
2025-09-05 09:14:24 +0200

    Diagram Séquence

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello World !')
 })
 
-app.get('/creator', (req, res) => {
-    res.send('Leroy was here !')
-})
-
 app.listen(port, () => {
     console.log(`Server started on port : ${port}`)
 })
---
+++ server.js
@@ -0,0 +1,3 @@
+import express from 'express'
+import { PORT } from '/config/env.js'
+import userRouter from '/routes/products.js'
